4c6a5e81b10889726b3fad46faf0f91bbaad24a9
Andi Bade <andi@galaxycats.com>
2010-04-02 13:42:33 +0200

    Message Test erstellt

--- statr/test/unit/message_test.rb
+++ statr/test/unit/message_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "should have a user" do
+    message = Message.create
+    assert message.errors[:user], "There should be an error on :user if no user is associated"
   end
+  
+  test "should have a content" do
+    message = Message.create
+    assert message.errors[:content], "There should be an error on :content if no content is set"
+  end
+  
+  test "should have a content with at least one character" do
+    message = Message.create(:content => "")
+    assert message.errors[:content], "There should be an error on :content if no content is set"
+    message = Message.create(:content => "12")
+    assert message.errors[:content].empty?, "There should be no error on :content if there is at least one character"
+  end
+  
 end
